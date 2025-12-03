8e2054a9d096883c1b6219ad5bbe2e0e890ed67b
Ethan Betts <you@example.com>
2025-12-03 23:42:38 +0100

    checkpoint

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import FaqListView
+from .views.faq_list_view import FaqListView
 
 urlpatterns = [
     path('faqs/', FaqListView.as_view(), name='faq-list'),
