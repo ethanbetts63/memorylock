4c8320d1bc4947607152642820f09fe9618b5341
Ethan Betts <you@example.com>
2025-12-11 15:36:19 +0100

    checkpoiknt

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,20 +4,22 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/static/',
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:8000',
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === 'production' ? '/static/' : '/',
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
       },
     },
-  },
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://127.0.0.1:8000',
+          changeOrigin: true,
+        },
+      },
+    },
+  }
 })
