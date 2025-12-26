4b409607c3802c1ece8d187ebef33dc2d83a7556
Ethan Betts <you@example.com>
2025-12-26 22:13:07 +0100

    checkpoint

--- frontend/src/components/Letter.tsx
+++ frontend/src/components/Letter.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Letter: React.FC = () => {
   return (
-    <section className="px-4 sm:px-0">
+    <section className="px-4 sm:px-0 pb-4 sm:pb-0">
       <div className="">
         <h2 className="text-center text-4xl md:text-5xl font-bold tracking-tight text-primary-foreground mb-4">
           Forgetfulness is <span className= "italic underline font-bold">expensive</span>.
